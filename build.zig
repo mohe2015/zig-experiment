210e319bdd8ca3866336b35e11c80232bbb12c97
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-06-17 23:48:50 +0200

    Like this glfw?

--- build.zig
+++ build.zig
@@ -14,6 +14,10 @@ pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("hello-world", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+    exe.addSystemIncludePath("/usr/include");
+    exe.addLibraryPath("/usr/lib/");
+    exe.linkSystemLibrary("glfw3");
+    exe.linkSystemLibrary("c");
     exe.install();
 
     const run_cmd = exe.run();
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,19 @@
 const std = @import("std");
+const v = @cImport(@cInclude("vulkan/vulkan.h"));
+const g = @cImport(@cInclude("GLFW/glfw3.h"));
+const warn = std.debug.warn;
+const panic = std.debug.panic;
+
+// https://ziglang.org/documentation/master/std/
+// https://ziglang.org/documentation/master/
 
 pub fn main() anyerror!void {
+    std.log.info("Hi.", .{});
+    if (g.glfwInit() == g.GL_FALSE) {
+        panic("Failed to initialize GLFW\n", .{});
+    }
+    defer g.glfwTerminate();
+
     std.log.info("All your codebase are belong to us.", .{});
 }
 
